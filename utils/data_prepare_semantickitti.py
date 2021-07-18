df5589fac7efb5cb475fb8aac22748d81e7c0237
Yingchen Nie <yingchennie@gmail.com>
2021-07-18 13:29:24 +1000

    further debugging;

--- utils/data_prepare_semantickitti.py
+++ utils/data_prepare_semantickitti.py
@@ -23,7 +23,9 @@ grid_size = 0.06
 dataset_path = TMPDIR + 'data/semantic_kitti/dataset/sequences'
 output_path = TMPDIR + 'data/semantic_kitti/dataset/sequences' + '_' + str(grid_size)
 seq_list = np.sort(os.listdir(dataset_path))
-
+count1 = 0
+count2 = 0
+count3 = 0
 for seq_id in seq_list:
     print('sequence' + seq_id + ' start')
     seq_path = join(dataset_path, seq_id)
@@ -41,14 +43,25 @@ for seq_id in seq_list:
         os.makedirs(label_path_out) if not exists(label_path_out) else None
         scan_list = np.sort(os.listdir(pc_path))
         for scan_id in scan_list:
+            """
             if int(scan_id[:6]) % 500 == 0:
                 print(scan_id)
+            """
             points = DP.load_pc_kitti(join(pc_path, scan_id))
             labels = DP.load_label_kitti(join(label_path, str(scan_id[:-4]) + '.label'), remap_lut)
             sub_points, sub_labels = DP.grid_sub_sampling(points, labels=labels, grid_size=grid_size)
             if sub_points.shape[0] < 4096*3:
-                print(f'SEQ is {seq_id} and NUM is {scan_id}')
+                print(f'SEQ {seq_id} NUM {scan_id} < 4096*3')
                 print(f'Original length is {points.shape[0]} and processed length is {sub_points.shape[0]}')
+                count1 += 1
+            if sub_points.shape[0] < 4096*4:
+                print(f'SEQ {seq_id} NUM {scan_id} < 4096*4')
+                print(f'Original length is {points.shape[0]} and processed length is {sub_points.shape[0]}')
+                count2 += 1
+            if sub_points.shape[0] < 4096*5:
+                print(f'SEQ {seq_id} NUM {scan_id} < 4096*5')
+                print(f'Original length is {points.shape[0]} and processed length is {sub_points.shape[0]}')
+                count3 += 1
             search_tree = KDTree(sub_points)
             KDTree_save = join(KDTree_path_out, str(scan_id[:-4]) + '.pkl')
             np.save(join(pc_path_out, scan_id)[:-4], sub_points)
@@ -68,10 +81,24 @@ for seq_id in seq_list:
         os.makedirs(proj_path) if not exists(proj_path) else None
         scan_list = np.sort(os.listdir(pc_path))
         for scan_id in scan_list:
+            """
             if int(scan_id[:6]) % 500 == 0:
                 print(scan_id)
+            """
             points = DP.load_pc_kitti(join(pc_path, scan_id))
             sub_points = DP.grid_sub_sampling(points, grid_size=0.06)
+            if sub_points.shape[0] < 4096*3:
+                print(f'SEQ {seq_id} NUM {scan_id} < 4096*3')
+                print(f'Original length is {points.shape[0]} and processed length is {sub_points.shape[0]}')
+                count1 += 1
+            if sub_points.shape[0] < 4096*4:
+                print(f'SEQ {seq_id} NUM {scan_id} < 4096*4')
+                print(f'Original length is {points.shape[0]} and processed length is {sub_points.shape[0]}')
+                count2 += 1
+            if sub_points.shape[0] < 4096*5:
+                print(f'SEQ {seq_id} NUM {scan_id} < 4096*5')
+                print(f'Original length is {points.shape[0]} and processed length is {sub_points.shape[0]}')
+                count3 += 1
             search_tree = KDTree(sub_points)
             proj_inds = np.squeeze(search_tree.query(points, return_distance=False))
             proj_inds = proj_inds.astype(np.int32)
@@ -82,3 +109,5 @@ for seq_id in seq_list:
                 pickle.dump(search_tree, f)
             with open(proj_save, 'wb') as f:
                 pickle.dump([proj_inds], f)
+
+print(f'After preprocessing, {count1} PCs are smaller than 4096*3, {count2} PCs are smaller than 4096*4, {count3} PCs are smaller than 4096*5.')
