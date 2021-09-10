bb99c15fefd1a99fce9304a5b29ef23b9556ae19
Yingchen Nie <yingchennie@gmail.com>
2021-09-10 12:53:02 +1000

    removing linux import for open3d for now

---
+++ install_requirements.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+conda install numpy=1.19
+
+conda install scikit-learn
+
+conda install -c anaconda pyyaml
+
+conda install -c conda-forge tensorflow=2.1.0
+
+pip3 install open3d-python
+
+conda install -c open3d-admin open3d=0.9.0
