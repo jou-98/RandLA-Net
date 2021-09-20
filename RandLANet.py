3e5a1c367cb9368ad0f8c875a8708d31dc41fcf4
Yingchen Nie <yingchennie@gmail.com>
2021-09-20 17:26:26 +1000

    a

--- RandLANet.py
+++ RandLANet.py
@@ -16,11 +16,11 @@ def log_out(out_str, f_out):
     print(out_str)
 
 def dice_coef(y_true, y_pred, smooth=1.0):
-    y_pred = tf.math.argmax(y_pred, axis=1, output_type=tf.dtypes.float32)
+    y_pred = tf.math.argmax(y_pred, axis=1, output_type=tf.dtypes.int32)
     y_true_f = tf.reshape(y_true, (1,-1))
     y_pred_f = tf.reshape(y_pred, (1,-1))
     # Change: Adding typecast to hopefully avoid error
-    #y_true_f = tf.cast(y_true_f,tf.float32)
+    y_true_f = tf.cast(y_true_f,tf.float32)
     #y_pred_f = tf.cast(y_pred_f,tf.float32)
     intersection = tf.math.reduce_sum(y_true_f * y_pred_f) 
     return (2. * intersection + smooth) / (
