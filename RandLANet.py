130ac53d431689d83e546a213cc67a34ee368cf3
Yingchen Nie <yingchennie@gmail.com>
2021-10-04 00:06:21 +1100

    a

--- RandLANet.py
+++ RandLANet.py
@@ -287,6 +287,8 @@ class Network:
         # Simple
         y_true = tf.cast(y_true, tf.float32)
         y_pred = tf.math.sigmoid(y_pred)
+        tf.print(y_true.shape)
+        tf.print(y_pred.shape)
         numerator = 2 * tf.reduce_sum(y_true * y_pred)
         denominator = tf.reduce_sum(y_true + y_pred)
 
